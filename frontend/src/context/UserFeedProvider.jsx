7113f5a5758828e84dd035caed44a33afb3c10e3
YashRajput09 <rajputyash8561@gmail.com>
2026-04-26 09:11:34 +0530

    fix: admin follow/unfollow

--- frontend/src/context/UserFeedProvider.jsx
+++ frontend/src/context/UserFeedProvider.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { createContext, useContext, useState, useEffect } from "react";
+import { useAuth } from "./AuthProvider";
+import toast from "react-hot-toast";
 // import SubmitBtnLoader from "../loaders/SubmitBtnLoader";
 
 const UserFeedContext = createContext();
@@ -8,6 +10,7 @@ export const UserFeedProvider = ({ children }) => {
   const [follow, setFollow] = useState([]);
   const [followers, setFollowers] = useState([]);
   const [followings, setFollowings] = useState([]);
+  const {isAuthenticated} = useAuth();
   // const [loader, setLoader] = useState(false);
 
   // load following list on mount
@@ -18,7 +21,8 @@ export const UserFeedProvider = ({ children }) => {
           `${import.meta.env.VITE_APP_BACKEND_URL}/user/following`,
           { withCredentials: true }
         );
-        setFollow(res.data.following); // Populate follow state
+
+        setFollow(res.data.following.map(user => user._id)); // Populate follow state
       } catch (error) {
         console.log("Error loading following list:", error);
       }
@@ -30,6 +34,7 @@ export const UserFeedProvider = ({ children }) => {
     // setLoader(true);
     try {
       // console.log(currentUserId);
+      if(!isAuthenticated) return toast.error("You have to login first.")
       await axios.post(
         `${import.meta.env.VITE_APP_BACKEND_URL}/user/follow/${userToFollowId}`,
         { currentUserId },
